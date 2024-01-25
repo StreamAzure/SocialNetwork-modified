d6220fe1af80a95c231621c482578e9001b7f44c
StreamAzure <1905105036@qq.com>
2024-01-25 16:03:56 +0800

    post_store 加 2s 延迟，保证在其他两个操作之后执行

--- src/ComposePostService/ComposePostHandler.h
+++ src/ComposePostService/ComposePostHandler.h
@@ -261,6 +261,7 @@ void ComposePostHandler::_UploadPostHelper(
     int64_t req_id, const Post &post,
     const std::map<std::string, std::string> &carrier) {
   TextMapReader reader(carrier);
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   auto parent_span = opentracing::Tracer::Global()->Extract(reader);
   auto span = opentracing::Tracer::Global()->StartSpan(
       "store_post_client", {opentracing::ChildOf(parent_span->get())});
