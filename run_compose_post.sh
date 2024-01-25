659a37c969fad41e5ce775d8febdfb85b620e97f
StreamAzure <1905105036@qq.com>
2024-01-25 15:08:33 +0800

    workload脚本

--- run_compose_post.sh
+++ run_compose_post.sh
@@ -1,1 +1,1 @@
-../wrk2/wrk -D exp -t 3 -c 3 -d 60 -L -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R 1
+../wrk2/wrk -D exp -t 1 -c 1 -d 20 -L -s ./wrk2/scripts/social-network/compose-post.lua http://localhost:8080/wrk2-api/post/compose -R 1
